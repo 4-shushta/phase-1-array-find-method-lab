d0dc25674e4d1288cd55a7d15dbc0e0f28654300
4-shushta <knucksslimey.gmail.com>
2024-05-03 15:11:00 +0300

    phase-1-array-find-method-lab complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(records){
+    const win = records.find(record => record.result === "W");
+    const win = records.find(({result}) => result === "W");
+    if (win){
+        return win.year 
+    } else {undefined}
+    } else {
+        return undefined
+    }
+
+
